c8bdfc32b6955620536758ade01cc56b8ee03eb9
jolivares <juan.o.lazcano@gmail.com>
2014-12-01 18:04:39 +0100

    clean up

--- ZeitgeistLogger.py
+++ ZeitgeistLogger.py
@@ -6,17 +6,16 @@ LOGGER_CMD = 'zeitgeist-logger'
 LOGGED_FLAG = 'logged-to-zeitgeist'
 
 def _log_file(view):
-	print("Flag " + view.get_status(LOGGED_FLAG))
-	if view.file_name() != None and view.get_status(LOGGED_FLAG) == None:		
+	if view.file_name() != None and view.get_status(LOGGED_FLAG) == "":		
 		name = basename(view.file_name())
 		uri = view.file_name()
 
-		print("name: %s file_name: %s " % (name, uri))
+		print("name: {0} file_name: {1} ".format(name, uri))
 
 		out = subprocess.Popen([_load_CMD(), name, uri],
 								stdout=subprocess.PIPE).communicate()[0]
 		view.set_status(LOGGED_FLAG, 'OK')
-		print("zeitgeist-logger outcome: %s" % out)
+		print("zeitgeist-logger outcome: {0}".format(out))
 
 def _load_CMD():
 	working_path = join(abspath(dirname(__file__)), LOGGER_CMD)
